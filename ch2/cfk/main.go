a1e9418b5d015aa8fae54383758c9a8c6559324f
Mikael Kundert <mikael.kundert@wunder.io>
2019-04-01 20:45:38 +0300

    Exercise 2.2: Rename program to cfk
    cf program standed for celsius and fahrenheit. Now we have third
    scale, so we must add that too for the program!

--- ch2/cfk/main.go
+++ ch2/cfk/main.go
@@ -19,7 +19,7 @@ func main() {
 	for _, arg := range os.Args[1:] {
 		t, err := strconv.ParseFloat(arg, 64)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+			fmt.Fprintf(os.Stderr, "cfk: %v\n", err)
 			os.Exit(1)
 		}
 		f := tempconv.Fahrenheit(t)
