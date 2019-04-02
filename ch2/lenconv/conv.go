4440e9dade9ff6ab0e9947caaf90e2ce0fb5fed8
Mikael Kundert <mikael.kundert@wunder.io>
2019-04-02 18:33:03 +0300

    Exercise 2.2: Use constant for calculations

--- ch2/lenconv/conv.go
+++ ch2/lenconv/conv.go
@@ -1,7 +1,7 @@
 package lenconv
 
 // MToF converts meters to feets.
-func MToF(m Meter) Feet { return Feet(m * 3.28083) }
+func MToF(m Meter) Feet { return Feet(m * Multiplier) }
 
 // FToM converts feets to meters.
-func FToM(f Feet) Meter { return Meter(f / 3.28083) }
+func FToM(f Feet) Meter { return Meter(f / Multiplier) }
--- ch2/lenconv/lenconv.go
+++ ch2/lenconv/lenconv.go
@@ -8,5 +8,8 @@ type Meter float64
 // Feet is one of the distance types.
 type Feet float64
 
+// Multiplier specifies the ratio between meters to feet.
+const Multiplier = 3.28083
+
 func (m Meter) String() string { return fmt.Sprintf("%gm", m) }
 func (f Feet) String() string  { return fmt.Sprintf("%gft", f) }
