a0bc5382806ba9ecf93a8f312bda47165e87fd71
Mikael Kundert <mikael.kundert@wunder.io>
2019-04-02 19:49:00 +0300

    Exercise 2.2: Read from Stdin if no arguments

--- ch2/unitconv/main.go
+++ ch2/unitconv/main.go
@@ -2,15 +2,16 @@
 // License: https://creativecommons.org/licenses/by-nc-sa/4.0/
 
 // See page 43.
-//!+
 
 // Unitconv converts its numeric argument to Celsius, Fahrenheit and Kelvin.
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/mikaelkundert/golang-exercises/ch2/lenconv"
 	"github.com/mikaelkundert/golang-exercises/ch2/massconv"
@@ -18,31 +19,46 @@ import (
 )
 
 func main() {
-	for _, arg := range os.Args[1:] {
-		t, err := strconv.ParseFloat(arg, 64)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "unitconv: %v\n", err)
-			os.Exit(1)
+	if len(os.Args) > 1 {
+		for _, arg := range os.Args[1:] {
+			t, err := strconv.ParseFloat(arg, 64)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "unitconv: %v\n", err)
+				os.Exit(1)
+			}
+			printUnits(t)
 		}
-		fmt.Println("TEMPERATURES")
-		fmt.Println("============")
-		tempconv.Print(t)
-
-		fmt.Println()
-
-		fmt.Println("LENGTHS")
-		fmt.Println("=======")
-		lenconv.Print(t)
-
-		fmt.Println()
-
-		fmt.Println("MASS")
-		fmt.Println("====")
-		massconv.Print(t)
-
-		fmt.Println()
-
+	} else {
+		input := bufio.NewScanner(os.Stdin)
+		for input.Scan() {
+			for _, arg := range strings.Split(input.Text(), " ") {
+				t, err := strconv.ParseFloat(arg, 64)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "unitconv: %v\n", err)
+					os.Exit(1)
+				}
+				printUnits(t)
+			}
+		}
 	}
 }
 
-//!-
+func printUnits(t float64) {
+	fmt.Println("TEMPERATURES")
+	fmt.Println("============")
+	tempconv.Print(t)
+
+	fmt.Println()
+
+	fmt.Println("LENGTHS")
+	fmt.Println("=======")
+	lenconv.Print(t)
+
+	fmt.Println()
+
+	fmt.Println("MASS")
+	fmt.Println("====")
+	massconv.Print(t)
+
+	fmt.Println()
+}
