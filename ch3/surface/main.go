fc614d8772dbc0861bcff716c3b04fb50165e51f
Mikael Kundert <mikael.kundert@wunder.io>
2019-04-12 21:31:40 +0300

    Exercise 3.4: Move drawing to its own function

--- ch3/surface/main.go
+++ ch3/surface/main.go
@@ -24,20 +24,26 @@ const (
 var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
 
 func main() {
-	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
+	fmt.Println(draw())
+}
+
+func draw() string {
+	openTag := fmt.Sprintf("<svg xmlns='http://www.w3.org/2000/svg' "+
 		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
 		"width='%d' height='%d'>", width, height)
+	var polygons string
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
 			ax, ay := corner(i+1, j)
 			bx, by := corner(i, j)
 			cx, cy := corner(i, j+1)
 			dx, dy := corner(i+1, j+1)
-			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+			polygons += fmt.Sprintf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
 				ax, ay, bx, by, cx, cy, dx, dy)
 		}
 	}
-	fmt.Println("</svg>")
+	closeTag := fmt.Sprintln("</svg>")
+	return openTag + polygons + closeTag
 }
 
 func corner(i, j int) (float64, float64) {
