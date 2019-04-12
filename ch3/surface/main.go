74b107698661f7858cfdb2adcb97f1e7da88ed4d
Mikael Kundert <mikael.kundert@wunder.io>
2019-04-12 21:57:37 +0300

    Exercise 3.4: Create HTTP server
    This will create a HTTP server when first argument is equal to "web",
    otherwise it will output the drawing to Stdout.

--- ch3/surface/main.go
+++ ch3/surface/main.go
@@ -9,7 +9,11 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"math"
+	"net/http"
+	"os"
 )
 
 const (
@@ -24,26 +28,34 @@ const (
 var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
 
 func main() {
-	fmt.Println(draw())
+	if len(os.Args) > 1 && os.Args[1] == "web" {
+		http.HandleFunc("/", handler)
+		log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	} else {
+		draw(os.Stdout)
+	}
 }
 
-func draw() string {
-	openTag := fmt.Sprintf("<svg xmlns='http://www.w3.org/2000/svg' "+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "image/svg+xml")
+	draw(w)
+}
+
+func draw(w io.Writer) {
+	fmt.Fprintf(w, "<svg xmlns='http://www.w3.org/2000/svg' "+
 		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
 		"width='%d' height='%d'>", width, height)
-	var polygons string
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
 			ax, ay := corner(i+1, j)
 			bx, by := corner(i, j)
 			cx, cy := corner(i, j+1)
 			dx, dy := corner(i+1, j+1)
-			polygons += fmt.Sprintf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+			fmt.Fprintf(w, "<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
 				ax, ay, bx, by, cx, cy, dx, dy)
 		}
 	}
-	closeTag := fmt.Sprintln("</svg>")
-	return openTag + polygons + closeTag
+	fmt.Fprintln(w, "</svg>")
 }
 
 func corner(i, j int) (float64, float64) {
